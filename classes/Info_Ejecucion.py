1e60f7eafed90be6a8dabc6d3081a38dc845a852
Diego1403 <Diegovr.1403@gmail.com>
2023-11-06 13:04:32 +0100

    implementado ox2

--- classes/Info_Ejecucion.py
+++ classes/Info_Ejecucion.py
@@ -1,3 +1,4 @@
+import configparser
 import math
 import numpy as np
 
@@ -25,4 +26,33 @@ class Info_Ejecucion:
         for i in range(self.dimension):
             for j in range(self.dimension):
                 self.matriz_distancias[i][j] = distancia_euclidea(self.nodos[i],self.nodos[j])
+        return self.matriz_distancias
+                
+    def print_matriz_distancias(self):
+        for row in self.matriz_distancias:
+            print (row)
     
+    def load_configuration(self,file_path):
+        # Initialize the parser
+        config = configparser.ConfigParser()
+
+        # Read the configuration file
+        config.read(file_path)
+
+
+        # Read values from the 'default' section
+        try:
+
+            self.algoritmos= config.get('default', 'algoritmos')
+            self.dataset= config.get('default', 'dataset')
+            self.tam_poblacion= config.getint('default', 'M')
+            self.E = config.getint('default', 'E')
+            self.kBest= config.getint('default', 'kBest')
+            self.kWorst = config.getint('default', 'kWorst')
+            self.evaluaciones = config.getint('default', 'evaluaciones')
+            self.semilla = config.getint('default', 'semilla')
+            
+            print("config.ini loaded correctly")
+        except (configparser.NoOptionError, ValueError) as e:
+            print(f"An error occurred: {e}")
+            return None
--- main.py
+++ main.py
@@ -1,7 +1,67 @@
-import math
+import random
+import time
 import numpy as np
-from controladores.C_Archivos import read_tsp_file
+from controladores.C_Archivos import archivo_save_output, read_tsp_file
 from classes.Info_Ejecucion import  Info_Ejecucion
+
+def inicializar_poblacion(num_individuos, num_ciudades):
+    poblacion = []
+    for _ in range(num_individuos):
+        individuo = list(range(num_ciudades))
+        random.shuffle(individuo)
+        poblacion.append(individuo)
+    return poblacion
+
+def calcular_fitness(individuo, matriz_distancias):
+    return sum(matriz_distancias[individuo[i], individuo[(i+1) % len(individuo)]] for i in range(len(individuo)))
+
+def seleccion_torneo(poblacion, k, matriz_distancias):
+    seleccionados = random.sample(poblacion, k)
+    seleccionados.sort(key=lambda ind: calcular_fitness(ind, matriz_distancias))
+    return seleccionados[0]
+
+
+
+def cruzar_OX2(padre1, padre2):
+    hijo = [-1] * len(padre1)
+    start, end = sorted(random.sample(range(len(padre1)), 2))
+    hijo[start:end] = padre1[start:end]
+    pos = end
+    for gene in padre2:
+        if gene not in hijo:
+            if pos >= len(hijo):
+                pos = 0
+            hijo[pos] = gene
+            pos += 1
+    return hijo
+
+def mutar_2opt(individuo):
+    a, b = random.sample(range(len(individuo)), 2)
+    individuo[a], individuo[b] = individuo[b], individuo[a]
+    return individuo
+
+def algoritmo_genetico(matriz_distancias, num_generaciones, tam_poblacion):
+    poblacion = inicializar_poblacion(tam_poblacion, len(matriz_distancias))
+    start_time = time.time()
+    for _ in range(num_generaciones):
+        nueva_poblacion = []
+        k = 3
+        
+        while len(nueva_poblacion) < tam_poblacion:
+            padre1 = seleccion_torneo(poblacion, k, matriz_distancias)
+            padre2 = seleccion_torneo(poblacion, k, matriz_distancias)
+            hijo = cruzar_OX2(padre1, padre2)
+            if random.random() < 0.1:  # Probabilidad de mutación
+                hijo = mutar_2opt(hijo)
+            nueva_poblacion.append(hijo)
+        poblacion = nueva_poblacion
+        
+        if time.time() - start_time > 60:
+            print("Tiempo límite alcanzado, terminando evolución.")
+            return poblacion
+        
+        
+    return poblacion
 
 if __name__ == "__main__":
     #cargamos informacion 
@@ -14,9 +74,18 @@ if __name__ == "__main__":
                          # esto no usa la clase nodo (no hace falta)
     
     #matriz distancias
+    
+    IE.load_configuration("config.ini")
+    print(IE.evaluaciones)
     IE.calcular_matriz_distancias()
     
 
-
-    #pass
+    poblacion = algoritmo_genetico(IE.matriz_distancias, IE.evaluaciones, 50)
+    
+    # Encontrar la mejor solución
+    mejor_solucion = min(poblacion, key=lambda individuo: calcular_fitness(individuo, IE.matriz_distancias))
+    mejor_distancia = calcular_fitness(mejor_solucion, IE.matriz_distancias)
+    
+    print("Mejor solución encontrada:", mejor_solucion)
+    print("Distancia de la mejor solución:", mejor_distancia)
 
